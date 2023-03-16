ed0a22a32ce243857b150c2d56b8acaa064ca4f5
rpottanat <rpottanat@gmail.com>
2023-03-16 23:27:28 +1100

    initial commit console

---
+++ name-sorter/App.cs
@@ -0,0 +1,34 @@
+﻿using name_sorter_library.Service;
+
+namespace name_sorter
+{
+    public class App
+    {
+        private readonly IFileService _fileService;
+        private const string OutPutFileName = $"Document\\sorted-names-list.txt";
+        private const string filePath = $"Document\\unsorted-names-list.txt";
+
+        public App(IFileService fileService )
+        {
+            _fileService = fileService;
+        }
+
+        internal async Task Run(string[] args)
+        {
+           // string filePath = args[0];           
+
+          //  filePath = $"Document\\{filePath}";
+
+            var names = await _fileService.ReadFromFile(filePath);
+
+            _ = await _fileService.WriteToFile(OutPutFileName, names);
+
+            names = await _fileService.ReadFromFile(OutPutFileName);
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.ToString());
+            }
+        }
+    }
+}
---
+++ name-sorter/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using name_sorter;
+using name_sorter_library.Service;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+   await services.GetRequiredService<App>().Run(args);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+         .ConfigureAppConfiguration(config =>
+         {
+             config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+         })
+         .ConfigureServices(services =>
+         {
+             services.AddScoped<IFileService, FileService>();    
+             services.AddSingleton<App>(); 
+         });
+}
