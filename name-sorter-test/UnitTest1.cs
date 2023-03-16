e0a5c30f68902efe2fa5e433bf2f3b03249ed764
rpottanat <rpottanat@gmail.com>
2023-03-16 23:29:29 +1100

    initial commit test project

---
+++ name-sorter-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace name_sorter_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
